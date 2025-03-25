9bb0c97c095b11d0f1d2a2e786293cf00cc1b3ea
Surya Prakash <surya.forum@gmail.com>
2025-03-25 23:52:46 +0530

    Test

---
+++ Module 2/Topic 1/Test.java
@@ -0,0 +1,7 @@
+class Test {
+    public static void main(String[] args) {
+        int age = 14;
+        assert age <= 18 : "Cannot Vote";
+        System.out.println("The voter's age is " + age);
+    }
+}
